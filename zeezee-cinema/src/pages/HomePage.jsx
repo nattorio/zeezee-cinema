245bd3ca27df9a28d78db9265a3fda06736818c0
MinKyeonghyeon <28599@naver.com>
2025-08-21 10:47:29 +0900

    feat/카드 컴포넌트 설정

---
+++ zeezee-cinema/src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import Carousel from '../component/MovieList.jsx/carousel';
+import MovieList from '../component/MovieList.jsx/MovieList';
+function HomePage() {
+    return (
+        <>
+            <h1>homepage</h1>
+            <Carousel />
+            <MovieList />
+            <MovieList />
+            <MovieList />
+        </>
+    );
+}
+
+export default HomePage;
